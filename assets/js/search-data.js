dea725dfed317f6caf49e518a693ea4f76e19e61
47z1Lu7h <86801785+47z1Lu7h@users.noreply.github.com>
2025-01-03 16:05:43 +0000

    Deploying to gh-pages from @ 47z1Lu7h/47z1lu7h.github.io@0fef7620be968729941f7165683b8986f6780d8b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-awake",
+          title: "awake",
+          description: "A growing collection of things that make you thing/awake... if you want ☺️ 😜",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/awakening/";
+          },
         },{id: "nav-pentest",
           title: "pentest",
           description: "A growing collection of hacking stuff...",
