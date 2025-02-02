a0f086417074bc7accf4c4d7145b6944c7928cc3
47z1Lu7h <86801785+47z1Lu7h@users.noreply.github.com>
2025-02-02 14:16:56 +0000

    Deploying to gh-pages from @ 47z1Lu7h/47z1lu7h.github.io@c1a2360fef8bf86ddb3688b1c5ab031dd74ef777 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "";
               },
-            },{id: "dropdown-hacing",
-              title: "hacing",
+            },{id: "dropdown-hacking",
+              title: "hacking",
               description: "",
               section: "Dropdown",
               handler: () => {
@@ -73,37 +73,158 @@ ninja.data = [{
           window.location.href = "/blog/2025/how-to-aproach-CTF's/";
         
       },
-    },{id: "post-a-post-with-images",
+    },{id: "post-smt",
       
-        title: "a post with images",
+        title: "smt",
       
-      description: "this is what included images could look like",
+      description: "Transmitting a file from your computer to another computer.",
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2025/images/";
+          window.location.href = "/blog/2025/satisfiability-modulo-theories-smt-z3/";
         
       },
-    },{id: "post-a-post-with-discus-commen",
+    },{id: "post-reversing-tools",
       
-        title: "a post with discus commen",
+        title: "reversing tools",
       
-      description: "an example of a blog post with giscus comments",
+      description: "Transmitting a file from your computer to another computer.",
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2024/discus-comments/";
+          window.location.href = "/blog/2025/reversing-tools/";
         
       },
-    },{id: "post-a-post-with-image-galleries",
+    },{id: "post-file-upload",
       
-        title: "a post with image galleries",
+        title: "file-upload",
       
-      description: "this is what included image galleries could look like",
+      description: "Transmitting a file from your computer to another computer.",
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2024/photo-gallery/";
+          window.location.href = "/blog/2025/cheat-engine/";
+        
+      },
+    },{id: "post-file-upload",
+      
+        title: "file-upload",
+      
+      description: "Transmitting a file from your computer to another computer.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/blobrunner/";
+        
+      },
+    },{id: "post-shells-windows",
+      
+        title: "Shells - Windows",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/windows/";
+        
+      },
+    },{id: "post-reverse-shells",
+      
+        title: "reverse shells",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/reverse-shell/";
+        
+      },
+    },{id: "post-msfvenom",
+      
+        title: "MSFVenom",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/msfvenom/";
+        
+      },
+    },{id: "post-shells-linux",
+      
+        title: "Shells - Linux",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/linux/";
+        
+      },
+    },{id: "post-fully-ttys",
+      
+        title: "fully ttys",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/full-ttys/";
+        
+      },
+    },{id: "post-expose-local-to-the-internet",
+      
+        title: "Expose local to the internet",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/expose-local-to-the-internet/";
+        
+      },
+    },{id: "post-xxe-xee-xml-external-entity",
+      
+        title: "XXE - XEE - XML External Entity",
+      
+      description: "Transmitting a file from your computer to another computer.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/xxe-xee-xml-external-entity/";
+        
+      },
+    },{id: "post-xssi",
+      
+        title: "XSSI",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/xssi-cross-site-script-inclusion/";
+        
+      },
+    },{id: "post-ldap-injection",
+      
+        title: "LDAP Injection",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/ldap-injection/";
+        
+      },
+    },{id: "post-idor",
+      
+        title: "Idor",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/idor/";
         
       },
     },{id: "post-write-up-machine-keeper-hackthebox-easy",
@@ -117,17 +238,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/writeUp-keeper/";
         
       },
-    },{id: "post-a-post-with-tabs",
-      
-        title: "a post with tabs",
-      
-      description: "this is what included tabs in a post could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/tabs/";
-        
-      },
     },{id: "post-how-to-play-hackthebox-com",
       
         title: "how to play hackthebox.com",
@@ -137,281 +247,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2024/how-to-play-hackthebox/";
-        
-      },
-    },{id: "post-a-post-with-typograms",
-      
-        title: "a post with typograms",
-      
-      description: "this is what included typograms code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/typograms/";
-        
-      },
-    },{id: "post-a-post-that-can-be-cited",
-      
-        title: "a post that can be cited",
-      
-      description: "this is what a post that can be cited looks like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/post-citation/";
-        
-      },
-    },{id: "post-a-post-with-videos",
-      
-        title: "a post with videos",
-      
-      description: "this is what included videos could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/videos/";
-        
-      },
-    },{id: "post-a-post-with-pseudo-code",
-      
-        title: "a post with pseudo code",
-      
-      description: "this is what included pseudo code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/pseudocode/";
-        
-      },
-    },{id: "post-a-post-with-code-diff",
-      
-        title: "a post with code diff",
-      
-      description: "this is how you can display code diffs",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/code-diff/";
-        
-      },
-    },{id: "post-a-post-with-advanced-image-components",
-      
-        title: "a post with advanced image components",
-      
-      description: "this is what advanced image components could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/advanced-images/";
-        
-      },
-    },{id: "post-a-post-with-vega-lite",
-      
-        title: "a post with vega lite",
-      
-      description: "this is what included vega lite code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/vega-lite/";
-        
-      },
-    },{id: "post-a-post-with-geojson",
-      
-        title: "a post with geojson",
-      
-      description: "this is what included geojson code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/geojson-map/";
-        
-      },
-    },{id: "post-a-post-with-echarts",
-      
-        title: "a post with echarts",
-      
-      description: "this is what included echarts code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/echarts/";
-        
-      },
-    },{id: "post-a-post-with-chart-js",
-      
-        title: "a post with chart.js",
-      
-      description: "this is what included chart.js code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/chartjs/";
-        
-      },
-    },{id: "post-a-post-with-bibliography",
-      
-        title: "a post with bibliography",
-      
-      description: "an example of a blog post with bibliography",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/post-bibliography/";
-        
-      },
-    },{id: "post-a-post-with-jupyter-notebook",
-      
-        title: "a post with jupyter notebook",
-      
-      description: "an example of a blog post with jupyter notebook",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/jupyter-notebook/";
-        
-      },
-    },{id: "post-a-post-with-custom-blockquotes",
-      
-        title: "a post with custom blockquotes",
-      
-      description: "an example of a blog post with custom blockquotes",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/custom-blockquotes/";
-        
-      },
-    },{id: "post-a-post-with-table-of-contents-on-a-sidebar",
-      
-        title: "a post with table of contents on a sidebar",
-      
-      description: "an example of a blog post with table of contents on a sidebar",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/sidebar-table-of-contents/";
-        
-      },
-    },{id: "post-a-post-with-audios",
-      
-        title: "a post with audios",
-      
-      description: "this is what included audios could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/audios/";
-        
-      },
-    },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
-      
-        title: "displaying beautiful tables with Bootstrap Tables",
-      
-      description: "an example of how to use Bootstrap Tables",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/tables/";
-        
-      },
-    },{id: "post-a-post-with-table-of-contents",
-      
-        title: "a post with table of contents",
-      
-      description: "an example of a blog post with table of contents",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/table-of-contents/";
-        
-      },
-    },{id: "post-a-post-with-redirect",
-      
-        title: "a post with redirect",
-      
-      description: "you can also redirect to assets like pdf",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/assets/pdf/example_pdf.pdf";
-        
-      },
-    },{id: "post-a-post-with-diagrams",
-      
-        title: "a post with diagrams",
-      
-      description: "an example of a blog post with diagrams",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2021/diagrams/";
-        
-      },
-    },{id: "post-a-distill-style-blog-post",
-      
-        title: "a distill-style blog post",
-      
-      description: "an example of a distill-style blog post and main elements",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2021/distill/";
-        
-      },
-    },{id: "post-cats",
-      
-        title: "Cats",
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/posts/code";
-        
-      },
-    },{id: "post-a-post-with-twitter",
-      
-        title: "a post with twitter",
-      
-      description: "an example of a blog post with twitter",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2020/twitter/";
-        
-      },
-    },{id: "post-a-post-with-disqus-comments",
-      
-        title: "a post with disqus comments",
-      
-      description: "an example of a blog post with disqus comments",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2015/disqus-comments/";
-        
-      },
-    },{id: "post-a-post-with-math",
-      
-        title: "a post with math",
-      
-      description: "an example of a blog post with some math",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2015/math/";
-        
-      },
-    },{id: "post-a-post-with-formatting-and-links",
-      
-        title: "a post with formatting and links",
-      
-      description: "march &amp; april, looking forward to summer",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2015/formatting-and-links/";
         
       },
     },{id: "news-science-of-the-heart",
@@ -449,8 +284,8 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/ActiveDirectory/";
-            },},{id: "projects-pent3st1ng",
-          title: 'Pent3st1ng',
+            },},{id: "projects-p3nt3st1ng",
+          title: 'P3nt3st1ng',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/AllAboutPentesting/";
